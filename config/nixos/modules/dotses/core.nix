15b425497169cdbbe0ff7f65785c8fb5dd2c127c
Andrea Falconi <andrea.falconi@gmail.com>
2019-12-27 12:12:27 +0100

    update nixos version & timezone; add new network stuff generated by 19.09 installer.

--- config/nixos/modules/dotses/core.nix
+++ config/nixos/modules/dotses/core.nix
@@ -21,9 +21,17 @@
 
   # Locale and time zone.
   i18n.defaultLocale = "en_GB.UTF-8";
-  time.timeZone = "Europe/Paris";
+  time.timeZone = "Europe/Zurich";
+
+  # Starting from NixOS 19.09, `useDHCP` should be specified separately for
+  # each I/F and the global `networking.useDHCP` gets deprecated. But the
+  # global `useDHCP` defaults to `true` so we'll have to fake the intended
+  # behaviour by setting it to `false` ourselves---this is what the NixOS
+  # installer does too when generating the initial system config.
+  networking.useDHCP = false;
+  networking.interfaces.enp0s3.useDHCP = true;
 
   # The NixOS release to be compatible with for stateful data such as databases.
-  system.stateVersion = "19.03";
+  system.stateVersion = "19.09";
 
 }
